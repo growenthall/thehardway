bfc27a607be410deaef79b4708b6a135b83b7506
Growenthall <zuchowski.mateusz@gmail.com>
2016-04-27 23:31:03 +0200

    new file

---
+++ ex3.rb
@@ -0,0 +1,8 @@
+puts "I will now count mi chickens"
+puts "Hens #{25+30/6}"
+puts "Roostrs #{100-25*3%4}"
+puts "Now I will count the eggs:"
+puts "3+2+1-5+4%2-1/4+6"
+puts "Is it true that 3+2<5-7?"
+puts 3+2<5-7
+
